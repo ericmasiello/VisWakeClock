46c20702473fd1c53f8f5580817684f269992683
Eric Masiello <emasiello@vistaprint.com>
2024-12-20 10:34:27 -0500

    Initial Commit

---
+++ VisWakeClock/VisWakeClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisWakeClockApp.swift
+//  VisWakeClock
+//
+//  Created by Eric Masiello on 12/20/24.
+//
+
+import SwiftUI
+
+@main
+struct VisWakeClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
